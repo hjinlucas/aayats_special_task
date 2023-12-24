ff31f3f1373ffe57fb8a4715efb2dccc269af1b2
Hao (Lucas) Jin <100209644+hjinlucas@users.noreply.github.com>
2023-12-23 21:18:06 -0800

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema(
         isCertified:{type: Boolean, default: false},//Certified user can have certification label
         isAgent:{type: Boolean, default: false},//Agent user can upload their studios for rent
         description:{type: String, require:true, default:"One line of words is not enough to describe my work"},
-        avatar:{type: String, required: true, default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.pinterest.com%2Fpin%2Fuser-pictures-by-anna-litviniuk--536702480572227623%2F&psig=AOvVaw18GFaDcplLUugSh3Rsdbx0&ust=1701453122699000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCIDv3vmk7IIDFQAAAAAdAAAAABAE"},
+        avatar:{type: String, required: true, default: "https://via.placeholder.com/150"},
     },  {timestamps: true}
 )
 
